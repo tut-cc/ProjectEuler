cc28971cd742f9cdda2221e106d578bef5fb0c90
Shingo Ohtsuka <capriciouswalk@gmail.com>
2016-05-22 03:22:40 +0900

    つまらなすぎるので変えたらいろいろつらさを発見

--- Problem9/d/main.d
+++ Problem9/d/main.d
@@ -1,21 +1,40 @@
-pragma(msg, solve);
+import std.typecons;
+import std.range;
+import std.array;
+import std.algorithm;
 
-int solve()
+auto solve()
 {
-  for (int a = 1; a < 1000; ++a) {
-    for (int b = a + 1; b < 1000; ++b) {
-      for (int c = b + 1; c < 1000; ++c) {
-        if (a*a + b*b == c*c && a + b + c == 1000) {
-          return a * b * c;
-        }
-      }
-    }
-  }
-  return -1;
+  return 1.iota(1000).map!(
+      a => zip(a.repeat.take(1000 - a) , (a+1).iota(1000)).map!(
+        b => zip(
+          b[0].repeat.take(1000 - b[1]),
+          b[1].repeat.take(1000 - b[1]),
+          (b[1]+1).iota(1000)
+          ).map!(
+            c => tuple(c[0]*c[0] + c[1]*c[1], c[2]*c[2], c[0]+c[1]+c[2], c[0]*c[1]*c[2])
+          ).filter!(t => t[0] == t[1])
+          .filter!(t => t[2] == 1000)
+          .map!(r => r[3])
+        ).filter!(r => !r.empty)
+      ).filter!(r => !r.empty);
 }
 
+// pragma(msg, solve)
 // dmd -c main.d => Error: out of memory
 
+// dmd -O -release -inline main.d
 import std.stdio;
-void main(){solve.writeln;}
+void main(){
+  solve.writeln;
+  /+ 死ぬ
+  10.iota.map!(
+      a=>10.iota.map!(
+        b=>10.iota.map!(
+          c => tuple(a, b, c)
+          )
+        )
+      ).writeln;
+  +/
+}
 
