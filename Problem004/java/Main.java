ab1ece6aa68beb2f8c257a8df0e0e4fdc1221995
foldori <origaminomiko@gmail.com>
2016-05-23 17:52:07 +0900

    I solved probelm004 by Java

---
+++ Problem004/java/Main.java
@@ -0,0 +1,20 @@
+class Main{
+	public static void main(String[] args){
+		System.out.println(isKaibun(1234321));
+		int max = 0;
+		for(int i=100;i<=999;i++){
+			for(int j=100;j<=999;j++){
+				max = (isKaibun(i*j) && i*j > max) ? i*j : max;
+			}
+		}
+		System.out.println(max);
+	}
+	
+	public static boolean isKaibun(int n){
+		String str = n+"";
+		for(int i=0;i<str.length()/2;i++){
+			if(str.charAt(i) != str.charAt(str.length() - i - 1))return false;
+		}
+		return true;
+	}
+}
