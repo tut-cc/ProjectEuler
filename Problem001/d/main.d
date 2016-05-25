4c687cf10ffd407e59f25a25f5532f884ac3b1e0
Kazuki Komatsu <enjouzensyou.boinc@gmail.com>
2016-05-25 10:20:52 +0900

    add an answer of Problem001 by D-runtime

--- Problem001/d/main.d
+++ Problem001/d/main.d
@@ -1,5 +1,18 @@
 import std.algorithm, std.range;
 
+// コンパイル時に解く場合
 pragma(msg, iota(1, 1000).filter!"(!(a%3) || !(a%5))".reduce!"a + b");
 
-void main(){}
+void main()
+{
+    import std.stdio;
+
+    // 実行時にC言語っぽく解く場合
+    uint sum;
+    foreach(uint i; 1 .. 1000){
+        if(i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+
+    writeln(sum);
+}
