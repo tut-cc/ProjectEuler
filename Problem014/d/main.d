17d42d6964c772d74629ad4c4ba8f08bdf8686c2
Kazuki Komatsu <enjouzensyou.boinc@gmail.com>
2016-05-29 23:46:15 +0900

    add an answer of Problem014 by D

---
+++ Problem014/d/main.d
@@ -0,0 +1,19 @@
+import std.algorithm,
+       std.range,
+       std.stdio,
+       std.typecons,
+       std.conv;
+
+
+uint solve()
+{
+    return iota(3, 1_000_000, 2)
+    .map!(a => a.tuple(a.to!ulong.recurrence!"a[n-1] % 2 == 0 ? a[n-1]/2 : a[n-1]*3+1".countUntil!"a==1"()))
+    .reduce!"a[1] < b[1] ? b : a"[0];
+}
+
+
+void main()
+{
+    writeln(solve());
+}
