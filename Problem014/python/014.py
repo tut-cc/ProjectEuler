d7f73830febb30e3117a8ab922f06046bca010ca
akahana-1 <h163341@edu.tut.ac.jp>
2016-06-01 10:44:33 +0900

    Add Problem 014 by Python

---
+++ Problem014/python/014.py
@@ -0,0 +1,18 @@
+N = int(1e6)
+seq = [None] * N
+for a in range(1, N + 1):
+    c = 1
+    n = a
+    while n != 1:
+        if n <= N and seq[n - 1]:
+            seq[a - 1] = c + seq[n - 1] - 1
+            break
+        if n & 1:
+            n = 3 * n + 1
+        else:
+            n = int(n / 2)
+        c += 1
+    else:
+        seq[a - 1] = c
+
+print(max(*((i + 1, a) for i, a in enumerate(seq)), key = lambda x : x[-1])[0])
